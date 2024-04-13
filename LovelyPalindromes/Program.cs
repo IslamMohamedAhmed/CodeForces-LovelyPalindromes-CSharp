6b2d823fa36550906dc037c4d634f68c9ec9eec7
unknown <islammohamedahmed49@gmail.com>
2024-04-13 13:06:54 +0200

    lp

---
+++ LovelyPalindromes/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+string str1 = ReadLine();
+char[] arr = str1.ToCharArray();
+Array.Reverse(arr);
+string str2 = new string(arr);
+WriteLine(str1+str2);
